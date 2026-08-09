45adc038b5246e24be3093dbfb41c5493e14cade
Glebsolopdf <geroin228339@yandex.ru>
2026-08-09 18:03:29 +0300

    fix: disable empty invite join

--- web-client/src/app/features/shell/invite-link.component.ts
+++ web-client/src/app/features/shell/invite-link.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NotificationStore } from '../../core/notifications/notification.store';
 import { parseInviteInput } from './invite-link';
 
@@ -11,14 +11,14 @@ import { parseInviteInput } from './invite-link';
     <section class="invite-link-section" aria-labelledby="invite-link-title">
       <h3 id="invite-link-title">У вас есть ссылка приглашения?</h3>
       <input id="invite-link" type="text" [formControl]="value" placeholder="Вставьте её сюда" autocomplete="off" spellcheck="false" aria-label="Ссылка или токен приглашения">
-      <button class="themed-button" type="button" (click)="submit()">Присоединиться</button>
+      <button class="themed-button" type="button" [disabled]="value.invalid" (click)="submit()">Присоединиться</button>
     </section>
   `,
 })
 export class InviteLinkComponent {
   private readonly notifications = inject(NotificationStore);
   readonly openInvite = output<string>();
-  protected readonly value = new FormControl('', { nonNullable: true });
+  protected readonly value = new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.pattern(/\S+/)] });
 
   protected submit(): void {
     const token = parseInviteInput(this.value.value, location.origin);
